9be4995726e8472294df5b6ff0d47469ff41c399
Ayoyinka-Sofuwa <68402767+Ayoyinka-Sofuwa@users.noreply.github.com>
2020-11-18 02:02:49 +0100

    Update train.py

--- train.py
+++ train.py
@@ -9,18 +9,15 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Dataset
 
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
+web_paths = ['https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv']
+ds = Dataset.Tabular.from_delimited_files(path=web_paths)
+### YOUR CODE HERE ###
 
 run = Run.get_context()
 
@@ -50,6 +47,14 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
+    return x_df, y_df
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+### YOUR CODE HERE ###a
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 42, test_size = 0.2)
 
 def main():
     # Add arguments to script
@@ -64,6 +69,9 @@ def main():
     run.log("Max iterations:", np.int(args.max_iter))
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(model, 'outputs/model.joblib')
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
